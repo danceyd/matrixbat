415450a8860d64ba8cf9ba30aa9987149d8a9be0
David <danceyd@tcd.ie>
2015-10-26 10:35:27 +0000

    first

---
+++ matrix.bat
@@ -0,0 +1,49 @@
+@echo off
+mode con: cols=80
+title Stay in Wonderland, and I will show you how deep the rabbit hole goes
+echo off
+color 06
+:tricks
+set /a num1=(10 * %random%) / 32768
+set /a num2=(10 * %random%) / 32768
+set /a num3=(10 * %random%) / 32768
+set /a num4=(10 * %random%) / 32768
+set /a num5=(10 * %random%) / 32768
+set /a num6=(10 * %random%) / 32768
+set /a num7=(10 * %random%) / 32768
+set /a num8=(10 * %random%) / 32768
+set /a num9=(10 * %random%) / 32768
+set /a num10=(10 * %random%) / 32768
+set /a num11=(10 * %random%) / 32768
+set /a num12=(10 * %random%) / 32768
+set /a num13=(10 * %random%) / 32768
+set /a num14=(10 * %random%) / 32768
+set /a num15=(10 * %random%) / 32768
+set /a num16=(10 * %random%) / 32768
+set /a num17=(10 * %random%) / 32768
+set /a num18=(10 * %random%) / 32768
+set /a num19=(10 * %random%) / 32768
+set /a num20=(10 * %random%) / 32768
+set /a num21=(10 * %random%) / 32768
+set /a num22=(10 * %random%) / 32768
+set /a num23=(10 * %random%) / 32768
+set /a num24=(10 * %random%) / 32768
+set /a num25=(10 * %random%) / 32768
+set /a num26=(10 * %random%) / 32768
+set /a num27=(10 * %random%) / 32768
+set /a num28=(10 * %random%) / 32768
+set /a num29=(10 * %random%) / 32768
+set /a num30=(10 * %random%) / 32768
+set /a num31=(10 * %random%) / 32768
+set /a num32=(10 * %random%) / 32768
+set /a num33=(10 * %random%) / 32768
+set /a num34=(10 * %random%) / 32768
+set /a num35=(10 * %random%) / 32768
+set /a num36=(10 * %random%) / 32768
+set /a num37=(10 * %random%) / 32768
+set /a num38=(10 * %random%) / 32768
+set /a num39=(10 * %random%) / 32768
+echo %num1% %num2% %num3% %num4% %num5% %num6% %num7% %num7% %num8% %num9% %num10% %num11% %num12% %num13% %num14% %num15% %num16% %num17% %num18% %num19% %num20% %num21% %num22% %num23% %num24% %num25% %num26% %num27% %num28% %num29% %num30% %num31% %num32% %num33% %num34% %num35% %num36% %num37% %num38% %num39%
+echo off
+goto tricks
+PAUSE
