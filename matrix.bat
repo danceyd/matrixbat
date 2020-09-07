25570ded2e0d7565554a7fa19e0695e30d33a721
David-Dancey-Lionbridge <david.dancey@lionbridge.com>
2020-09-07 16:02:10 +0100

    Changed width. Might change back later

--- matrix.bat
+++ matrix.bat
@@ -43,7 +43,7 @@ set /a num36=(10 * %random%) / 32768
 set /a num37=(10 * %random%) / 32768
 set /a num38=(10 * %random%) / 32768
 set /a num39=(10 * %random%) / 32768
-echo %num1% %num2% %num3% %num4% %num5% %num6% %num7% %num7% %num8% %num9% %num10% %num11% %num12% %num13% %num14% %num15% %num16% %num17% %num18% %num19% %num20% %num21% %num22% %num23% %num24% %num25% %num26% %num27% %num28% %num29% %num30% %num31% %num32% %num33% %num34% %num35% %num36% %num37% %num38% %num39%
+echo %num1% %num2% %num3% %num4% %num5% %num6% %num7% %num7% %num8% %num9% %num10% %num11% %num12% %num13% %num14% %num15% %num16% %num17% %num18% %num19% %num20% %num21% %num22% %num23% %num24% %num25% %num26% %num27% %num28% %num29% %num30% %num31% %num32% %num33% %num34% %num35% %num36% %num37% %num38% %num39% %num1% %num2% %num3% %num4% %num5% %num6% %num7% %num7% %num8% %num9% %num10% %num11% %num12% %num13% %num14% %num15% %num16% %num17% %num18% %num19% %num20% %num21% %num22% %num23% %num24% %num25% %num26% %num27% %num28% %num29% %num30% %num31% %num32% %num33% %num34% %num35% %num36% %num37% %num38% %num39% %num1% %num2% %num3% %num4% %num5% %num6% 
 echo off
 goto tricks
 PAUSE
